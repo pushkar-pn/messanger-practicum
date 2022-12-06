8a6f63d8c769ea2bac51fe8a0e125474bcd2b58a
pushkar-pn <pushkarpn@gmail.com>
2022-12-07 01:04:18 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// надо будет заменить на dist/
+app.use(express.static('./src/pages'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => console.log(`App listening to port ${port}. Go to http://localhost:${port}/index.html`));
